ccc2c87e62dbf7dce16a9545468ab4943ad532c4
Chen Eng C. <cuadradoengcs50@gmail.com>
2026-09-20 12:18:16 -0600

    Remove obsolete scripts and configs
    Remove unused helper and configuration files that were previously used for ad-hoc maintenance: eslint.config.js, postcss.config.js, fetch_schema.js/cjs, fix_syntax.cjs, update_jsx.cjs, update_edit_vehicle.cjs, update_es_json.cjs, update_es_json_edit.cjs. This cleans up deprecated scripts and redundant configs to reduce repository clutter.

--- fix_syntax.cjs
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('src/components/ui/table/QuerysDefinitions.tsx', 'utf8');
-
-content = content.replace('export const UPDATE_VEHICLE_MUTATION = gql\\', 'export const UPDATE_VEHICLE_MUTATION = gql');
-content = content.replace('}\\n\\;', '}\\n;');
-
-fs.writeFileSync('src/components/ui/table/QuerysDefinitions.tsx', content, 'utf8');
